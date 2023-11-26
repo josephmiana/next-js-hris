849fb5e6e8a2f32cf78badd61a824efc81f14978
lian <markliandperez61@gmail.comgit config --global user,name liangit config --global user.email markliandperez61@gmail.com>
2023-11-26 11:47:00 +0800

    new email and forgotpass added

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -77,30 +77,9 @@ export default function LoginPage() {
       <p>Welcome to WBMAJESTY</p>
 
       <form id="loginForm">
-        {selectedRole !== "Portal" && (
-          <div className="form-group">
-            <input
-              type="text"
-              placeholder="ID"
-              id="id"
-              name="ID"
-              value={bundy.employee_id}
-              onChange={(e) => setBundy({ ...bundy, employee_id: e.target.value })}
-              required
-            />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 448 512"
-              className="icon "
-              fill="white"
-            >
-              <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-            </svg>
-          </div>
-        )}
+     
 
-        {selectedRole !== "Attendance" && (
+        
           <div className="form-group">
             <input
               type="text"
@@ -122,9 +101,8 @@ export default function LoginPage() {
               <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
             </svg>
           </div>
-        )}
+ 
 
-        {selectedRole !== "Attendance" && (
           <div className="form-group">
             <input
               type="password"
@@ -135,18 +113,13 @@ export default function LoginPage() {
               onChange={(e) => setUser({ ...user, password: e.target.value })}
               required
             />
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="1em"
-              viewBox="0 0 448 512"
-              className="icon "
-              fill="black">
+          
             
-              <path d="M144 144v48H304V144c0-44.2-35.8-80-80-80s-80 35.8-80 80zM80 192V144C80 64.5 144.5 0 224 0s144 64.5 144 144v48h16c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V256c0-35.3 28.7-64 64-64H80z" />
-            </svg>
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="black"  className="icon "><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
           </div>
-        )}
+   
         <div className="form-group">
+        <p className="forgotpass">Forgot Password</p>
           {selectedRole !== "Attendance" && (
             <button type="button" onClick={onLogin} disabled={buttonDisabled}>
               Log in
