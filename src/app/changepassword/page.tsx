ed3501fdc4a92bf1bf823e8c435328ec61112f08
lian <markliandperez61@gmail.comgit config --global user,name liangit config --global user.email markliandperez61@gmail.com>
2023-11-27 15:00:45 +0800

    added status

--- src/app/changepassword/page.tsx
+++ src/app/changepassword/page.tsx
@@ -73,7 +73,7 @@ export default function LoginPage() {
 
   return (
     <div className="LoginForm">
-      <h1>{loading ? "Processing ..." : "Login to Your Account"}</h1>
+      <h1>{loading ? "Processing ..." : "Change Password"}</h1>
       <p>Welcome to WBMAJESTY</p>
 
       <form id="loginForm">
@@ -86,9 +86,6 @@ export default function LoginPage() {
               placeholder="username"
               id="username"
               name="username "
-              value={user.name}
-              onChange={(e) => setUser({ ...user, name: e.target.value })}
-              required
             />
 
             <svg
@@ -106,23 +103,34 @@ export default function LoginPage() {
           <div className="form-group">
             <input
               type="password"
-              placeholder="Password "
+              placeholder=" New Password "
               id="password"
               name="password"
-              value={user.password}
-              onChange={(e) => setUser({ ...user, password: e.target.value })}
-              required
+           required
             />
           
             
             <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="black"  className="icon "><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
           </div>
-   
+          
+          
+          <div className="form-group">
+            <input
+              type="password"
+              placeholder="Confirm Password "
+              id="password"
+              name="password"
+           required
+            />
+          
+            
+            <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="black"  className="icon "><path d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"/></svg>
+          </div>
         <div className="form-group">
-        <a href="/changepassword">  <p className="forgotpass">Forgot Password</p></a>
+        
           {selectedRole !== "Attendance" && (
             <button type="button" onClick={onLogin} disabled={buttonDisabled}>
-              Log in
+              Change Password
             </button>
           )}
           
@@ -130,17 +138,7 @@ export default function LoginPage() {
         <div className="error" id="errorMessage"></div>
       </form>
 
-      <div>
-        {selectedRole !== "Portal" && (
-          <div>
-            <button className="time" onClick={() => onLogInandOut()}>
-              Time
-            </button>
-            <p className="my-6">Time right now: {bundy.time}</p>
-          </div>
-        )}
-        {/* Render the current time */}
-      </div>
+    
     </div>
   );
 }
